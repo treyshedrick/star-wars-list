5a115fce3bfd43601fae608e2f7608427013e5a6
treyshedrick <treyshedrick@gmail.com>
2021-10-28 17:40:01 -0500

    fix: set unknown values to 0 in avg calculations

--- src/helpers/calculate.js
+++ src/helpers/calculate.js
@@ -1,26 +1,19 @@
-const usersMass = (users) => {
-    const massArr = users.map(user => (parseInt(user.mass.replace(/,/g,''))));
-    return massArr;
-}
-
-const usersHeight = users => {
-    const heightArr = users.map(user => (parseInt(user.height.replace(/,/g,''))));
-    return heightArr;
-}
-
 export const calculateAverage = (users, avgRequested) => {
-    const filteredUsers = users.filter(user => user[avgRequested] !== "unknown");
+    // maps numbers into own array and turns unknown values to 0 for average
+    const dataPoints = users.map(user => user[avgRequested]).map(number => {
+        if(number === "unknown"){
+            return 0;
+        }
+        return parseInt(number.replace(/,/g,''));
+    });
+
     let average;
 
-    if(filteredUsers.length === 0){
-        average = NaN
+    if(dataPoints.length === 0){
+        average = NaN;
     }
     // use reduce method to calculate average
-    if(avgRequested === 'mass' && filteredUsers.length > 0){
-        average = usersMass(filteredUsers).reduce((prev, curr) => (prev + curr)) / filteredUsers.length;
-    } else if(avgRequested === 'height' && filteredUsers.length > 0){
-        average = usersHeight(filteredUsers).reduce((prev, curr) => (prev + curr)) / filteredUsers.length;
-    }
+    average = dataPoints.reduce((prev, curr) => (prev + curr)) / dataPoints.length;
     
     return average
 }
